83a3299a9c7399451afd8e484fc311013c56f28f
JoyLynn <joylynnious@gmail.com>
2015-08-28 15:17:26 +0300

    added stock method to the product helper..

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,4 +3,12 @@ module ProductsHelper
   		number_to_currency price
 	end
 
+	def print_stock(stock)
+	  if stock > 0
+	  	<span class="in_stock">In Stock ()</span>
+	  else
+	  	<span class="out_stock">Out of Stock</span>
+	  end
+	end
+
 end
